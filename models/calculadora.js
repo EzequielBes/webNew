03cfd9fe90fb4fd43fcc2899a5f8b3fadbe26eb6
EzequielBes <ezequieltdbeserra@gmail.com>
2023-11-27 12:56:52 -0300

    create api status

---
+++ models/calculadora.js
@@ -0,0 +1,9 @@
+const somar = (n1, n2) => {
+  if (typeof numero1 !== "number" && typeof numero2 !== "number") {
+    return n1 + n2;
+  } else {
+    return error;
+  }
+};
+
+exports.somar = somar;
