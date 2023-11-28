fcad2230acbc4644448a634e60b1546cada5a790
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2023-11-28 18:46:12 +0000

    add `database.js` and `.env` files

--- models/calculadora.js
+++
@@ -1,9 +0,0 @@
-const somar = (n1, n2) => {
-  if (typeof numero1 !== "number" && typeof numero2 !== "number") {
-    return n1 + n2;
-  } else {
-    return error;
-  }
-};
-
-exports.somar = somar;
